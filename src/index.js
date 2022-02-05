b3a0c5d6e9bf440490bfb9b4e8dcc8b7be1fd383
Bryce Crist <brucecrusty@gmail.com>
2022-02-05 16:34:07 -0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const {Builder, By, Key, until} = require("selenium-webdriver");
 
-(async function test() {
+(async function example() {
     let driver = await new Builder()
         .forBrowser("chrome")
         .build()
 
     try {
-        await driver.get("https://www.google.com/ncr")
-        await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN)
-        await driver.wait(until.titleIs("webdriver - Google Search"), 1000)
+        await driver.get("https://www.google.com/ncr");
+        await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
+        await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
     } finally {
-        await driver.quit()
+        await driver.quit();
     }
-})()
+})();
